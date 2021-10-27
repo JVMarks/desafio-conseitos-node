5f288f0f40d9e6fd6060eb3be2d75f861361423b
João Victor Marks <jovic.marks@gmail.com>
2021-10-26 21:14:51 -0300

    Response

--- src/app.js
+++ src/app.js
@@ -1,33 +1,90 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repositorie)
+  return response.json(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex === -1) {
+    return response.status(400).json({ error: 'Repositorie not found' })
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes
+  }
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex >= 0) {
+    repositories.slice(repositorieIndex, 1);
+  } else{
+    return response.status(400).json({ error: 'Repositorie not found' })
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorie = repositories.find(repositorie => repositorie.id === id);
+
+  if (!repositorie) {
+    return response.status(400).send();
+  }
+
+  repositorie.likes ++;
+
+  return response.json(repositorie)
 });
 
 module.exports = app;
+
+
+/*
+
+yarn test
+yarn test --watchAll
+*/
